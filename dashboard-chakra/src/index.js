1363425a011a2040549d5fa079de3f7407751195
NickBritoDev <nicolasbcruz@gmail.com>
2023-07-11 11:58:36 -0300

    feat: rtl

--- dashboard-chakra/src/index.js
+++ dashboard-chakra/src/index.js
@@ -22,7 +22,7 @@ import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AuthLayout from "layouts/Auth.js";
 import AdminLayout from "layouts/Admin.js";
-import rtlLayout from "layouts/rtl.js";
+import rtlLayout from "layouts/RTL.js";
 
 ReactDOM.render(
   <HashRouter>
